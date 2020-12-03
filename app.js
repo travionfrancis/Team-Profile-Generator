b598ace79bbe439b2223a390a947ff5247f7168f
TraviFravi <travi1500@gmail.com>
2020-12-03 00:25:57 -0500

    I am not rewriting these damn questions

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const Choices = require("inquirer/lib/objects/choices");
 
 
 // Write code to use inquirer to gather information about the development team members,
@@ -16,22 +17,27 @@ const render = require("./lib/htmlRenderer");
 
 const employeeCatalog = []; 
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+// Asking questions to ask user about their status 
+const questions = [
+    {
+        type: "input",
+        name: "name",
+        message: "Name of the Employee?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "ID Number of the Employee?"
+    },
+    {
+        type: "input",
+        name: "email",
+        messages: "Email Address of the Employee?"
+    },
+    {
+        type: "list",
+        name: "role",
+        message: "Role of the Employee?",
+        choices: ["Engineer", "Intern", "Manager"]
+    }
+]
